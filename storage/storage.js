b8476ae3d1e9585cd197c166f64b3a2f44667760
mridul <mriduls.webosmotic@gmail.com>
2024-02-20 17:33:08 +0530

    realTime functionality added

--- storage/storage.js
+++ storage/storage.js
@@ -1,70 +1,65 @@
+// create methods to perform operations like save/edit/delete/add data
 
-
-  // create methods to perform operations like save/edit/delete/add data
-
-  export function setData(employeeData  /* should an array of obj */) {
-    try {
-      localStorage.setItem('employeeData', JSON.stringify(employeeData));
-      return { status: true };
-    } catch (error) {
-      return { status: false, error: error };
-    }
+export function setData(employeeData /* should an array of obj */) {
+  try {
+    localStorage.setItem('employeeData', JSON.stringify(employeeData));
+    return { status: true };
+  } catch (error) {
+    return { status: false, error: error };
   }
+}
 
-  export function getData() {
-    try {
-      const employeeData = JSON.parse(localStorage.getItem('employeeData')) ?? [];
-      return { status: true, employeeData: employeeData };
-    } catch (error) {
-      return { status: false, error: error };
-    }
+export function getData() {
+  try {
+    const employeeData = JSON.parse(localStorage.getItem('employeeData')) ?? [];
+    return { status: true, employeeData: employeeData };
+  } catch (error) {
+    return { status: false, error: error };
   }
+}
 
-  export function getSingleData(editRowId) {
-    try {
-      const storedData = getData().employeeData; //array of objects [{},{}]
+export function getSingleData(editRowId) {
+  try {
+    const storedData = getData().employeeData; //array of objects [{},{}]
 
-      const rowToUpdate = storedData.filter((val) => val.userId === editRowId);
+    const rowToUpdate = storedData.filter((val) => val.userId === editRowId);
 
-      return { status: true, rowToUpdate: rowToUpdate };
-    } catch (error) {
-      return { status: false, error: error };
-    }
+    return { status: true, rowToUpdate: rowToUpdate };
+  } catch (error) {
+    return { status: false, error: error };
   }
+}
 
-  export function editData(dataObj /*object {}*/) {
+export function editData(dataObj /*object {}*/) {
+  try {
+    const storedData = getData().employeeData; //array of objects [{},{}]
 
-    try {
-      const storedData = getData().employeeData; //array of objects [{},{}]
-
-      const rowId = storedData.findIndex((val) => val.userId === dataObj.userId);
-      storedData.splice(rowId, 1, dataObj);
-      localStorage.setItem('employeeData', JSON.stringify(storedData));
-      return { status: true };
-    } catch (error) {
-      return { status: false, error: error };
-    }
+    const rowId = storedData.findIndex((val) => val.userId === dataObj.userId);
+    storedData.splice(rowId, 1, dataObj);
+    localStorage.setItem('employeeData', JSON.stringify(storedData));
+    return { status: true };
+  } catch (error) {
+    return { status: false, error: error };
   }
-  export function deleteData(deleteRowId /*userId as a string*/) {
-    try {
-      const { employeeData } = getData(); //array of objects [{},{}]
+}
+export function deleteData(deleteRowId /*userId as a string*/) {
+  try {
+    const { employeeData } = getData(); //array of objects [{},{}]
 
-      const updatedData = employeeData.filter((val) => val.userId !== deleteRowId);
-      localStorage.setItem('employeeData', JSON.stringify(updatedData));
-      return { status: true };
-    } catch (error) {
-      return { status: false, error: error };
-    }
+    const updatedData = employeeData.filter((val) => val.userId !== deleteRowId);
+    localStorage.setItem('employeeData', JSON.stringify(updatedData));
+    return { status: true };
+  } catch (error) {
+    return { status: false, error: error };
   }
-  export function addData(dataObj /*object {}*/) {
-
-
-    try {
-      const { employeeData } = getData(); //array of objects [{},{}]
-      employeeData.push(dataObj);
-      localStorage.setItem('employeeData', JSON.stringify(employeeData));
-      return true;
-    } catch (error) {
-      return { status: false, error: error };
-    }
+}
+export function addData(dataObj /*object {}*/) {
+  try {
+    const { employeeData } = getData(); //array of objects [{},{}]
+    employeeData.push(dataObj);
+    localStorage.setItem('employeeData', JSON.stringify(employeeData));
+    return true;
+  } catch (error) {
+    return { status: false, error: error };
   }
+}
